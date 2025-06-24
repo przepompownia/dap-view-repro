de16f994414b306463c9c1f8ac3d2ccee683a14f
przepompownia <przepompownia@users.noreply.github.com>
2025-06-24 12:56:39 +0200

    Clean

--- bin/test.php
+++ bin/test.php
@@ -3,10 +3,3 @@
 if (!extension_loaded('xdebug')) {
     throw new Exception('Xdebug extension not loaded');
 };
-
-use Arctgx\Foo;
-
-require dirname(__DIR__) . '/vendor/autoload.php';
-
-$foo = new Foo();
-$foo->testBrk();
